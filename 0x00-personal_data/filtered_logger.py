bf9e14812487c4a4136c2ea81674ce21c68205c8
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-09 02:34:52 +0200

    pycodestyle

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,9 +6,9 @@ PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                    separator: str) -> str:
-        """ returns the log message obfuscated """
-        for field in fields:
-            message = re.sub(f'{field}=.*?{separator}',
-                            f'{field}={redaction}{separator}', message)
-        return message
+                 separator: str) -> str:
+    """ returns the log message obfuscated """
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
