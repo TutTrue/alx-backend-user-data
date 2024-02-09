373756cbfd93442f1a5b06cbc196cefee566d926
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-09 02:52:07 +0200

    get_logger function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,6 +15,19 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     return message
 
 
+
+def get_logger() -> logging.Logger:
+    """ returns a logging object """
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    return logger
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
