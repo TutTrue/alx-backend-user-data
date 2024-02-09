42a0bd7fc6e4cea4b365e5edf5155c6b8cdb8c5f
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-09 03:07:42 +0200

    hash_password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" encrypt password """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ returns a salted, hashed password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
