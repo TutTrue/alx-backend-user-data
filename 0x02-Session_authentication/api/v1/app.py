61a07499a6dd5ff88cac43e7b13d95465d92d772
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-15 19:48:40 +0200

    pycodestyle app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,7 +36,8 @@ def before_request():
                                             '/api/v1/unauthorized/',
                                             '/api/v1/forbidden/',
                                             '/api/v1/auth_session/login/']):
-            if not auth.authorization_header(request) and not auth.session_cookie(request):
+            if not auth.authorization_header(request) and \
+                  not auth.session_cookie(request):
                 abort(401)
             if not auth.current_user(request):
                 abort(403)
