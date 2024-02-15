6cbb279c2e17cfda08411cf828f5f0852d9b606b
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-15 20:09:01 +0200

    add session view

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -34,3 +34,17 @@ class SessionAuth(Auth):
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
         return User.get(user_id)
+
+    def destroy_session(self, request=None) -> bool:
+        """ Destroy a session
+        """
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+""" Module of session auth view
+"""
+from api.v1.views import app_views
+from flask import jsonify, request, abort
+from models.user import User
+from api.v1.auth.session_auth import SessionAuth
+
+
+sa = SessionAuth()
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def auth_session_login() -> str:
+    """ Auth session login
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or password is None:
+        return jsonify({"error": "email or password missing"}), 400
+    user = User.search({'email': email})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    user = user[0]
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    session_id = sa.create_session(user.id)
+    response = jsonify(user.to_json())
+    response.set_cookie('session_id', session_id)
+    return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def auth_session_logout() -> str:
+    """ Auth session logout
+    """
+    session_id = request.cookies.get('session_id')
+    if session_id is None:
+        return jsonify({"error": "session_id for cookie not found"}), 403
+    user = sa.current_user(request)
+    if not user:
+        return jsonify({"error": "no user found"}), 403
+    sa.destroy_session(request)
+    return jsonify({}), 200
+
+
+@app_views.route('/auth_session/profile', methods=['GET'],
+                 strict_slashes=False)
+def auth_session_profile() -> str:
+    """ Auth session profile
+    """
+    user = sa.current_user(request)
+    if not user:
+        return jsonify({"error": "no user found"}), 403
+    return jsonify(user.to_json()), 200
+
+
+@app_views.route('/auth_session/destroy', methods=['DELETE'],
+                 strict_slashes=False)
+def auth_session_destroy() -> str:
+    """ Auth session destroy
+    """
+    user = sa.current_user(request)
+    if not user:
+        return jsonify({"error": "no user found"}), 403
+    sa.destroy_session(request)
+    return jsonify({}), 200
