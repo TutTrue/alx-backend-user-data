200703dd42a137f34e34c0463ee6cc21cbcf882f
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-15 17:47:28 +0200

    auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Module of API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Class for API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method that require authentication
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Method that handles authorization header
+        """
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method that handles current user
+        """
+        return None
