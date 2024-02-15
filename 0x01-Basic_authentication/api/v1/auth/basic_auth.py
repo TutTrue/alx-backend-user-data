299d74d0cb69fd2edd613a6e3f0fb5d82bb5c8c5
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-15 18:12:29 +0200

    pycodestyle BasicAuth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,10 +8,11 @@ class BasicAuth(Auth):
     """ Class for Basic API authentication
     """
     def extract_base64_authorization_header(self,
-                                           authorization_header: str) -> str:
+                                            authorization_header: str) -> str:
         """ Method that extracts base64 authorization header
         """
-        if authorization_header is None or type(authorization_header) is not str:
+        if authorization_header is None or \
+                type(authorization_header) is not str:
             return None
         if authorization_header[:6] != 'Basic ':
             return None
