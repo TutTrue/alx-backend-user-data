5c6f73c03873b162acb56c6d8af7e20cdca65c88
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-15 18:34:43 +0200

    current_user method in BasicAuth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -60,3 +60,13 @@ class BasicAuth(Auth):
         if user is None or not user.is_valid_password(user_pwd):
             return None
         return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method that overloads Auth and retrieves current user
+        """
+        auth_header = self.authorization_header(request)
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        user_credentials = self.extract_user_credentials(decoded_header)
+        return self.user_object_from_credentials(user_credentials[0],
+                                                 user_credentials[1])
