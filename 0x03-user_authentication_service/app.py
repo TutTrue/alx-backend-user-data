6c60fefca0acf53a789068ec7cf454d941113bf9
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-22 21:03:47 +0200

    post user route in app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """basic flask app module"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -10,6 +14,18 @@ app = Flask(__name__)
 def index() -> str:
     """index route"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users',methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """create_user route"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
