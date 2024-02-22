8c9dd7307350f844aec5b210cc9b037d176295e0
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-22 21:24:14 +0200

    generate_uuid function in auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -12,6 +12,11 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
+def _generate_uuid(self) -> str:
+    """Generate a UUID."""
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -34,7 +39,3 @@ class Auth:
         except NoResultFound:
             return False
         return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
-
-    def _generate_uuid(self) -> str:
-        """Generate a UUID."""
-        return str(uuid.uuid4())
