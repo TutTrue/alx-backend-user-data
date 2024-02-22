4005d96b5170af0067cd1f9ea44e7a472092773c
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-22 03:30:38 +0200

    user shcema

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A class that can be used to represent a user."""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250))
+    hashed_password = Column(String(250))
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
