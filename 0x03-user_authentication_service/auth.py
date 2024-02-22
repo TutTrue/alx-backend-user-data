61ed9b19e07b3a1e7e009db02a9cd1d1f62f9af8
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-22 20:33:28 +0200

    Auth class and register_user function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 """Auth module"""
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """Returns a salted, hashed password."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> None:
+        """Register a user with email and password."""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            self._db.add_user(email, _hash_password(password))
+            return None
