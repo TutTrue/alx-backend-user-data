e3f4e3a3d15ed94314704a0fce0325d301d4e49f
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-23 03:00:18 +0200

    reset_password route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -61,5 +61,16 @@ def profile() -> str:
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/get_reset_password_token', methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """get_reset_password_token route"""
+    email = request.form.get('email')
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
