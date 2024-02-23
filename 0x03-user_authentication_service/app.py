83903a6b24b765e769710d3ba70eda13485e2311
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-23 02:51:20 +0200

    import redirect from flask

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """basic flask app module"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
