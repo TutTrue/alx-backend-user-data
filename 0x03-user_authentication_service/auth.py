ce56f1a729d2691d0bb237716a393badea6b4f8e
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-23 02:55:29 +0200

    create reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -63,3 +63,13 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """Destroy a session."""
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Get a password reset token."""
+        try:
+            user = self._db.find_user_by(email=email)
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        except NoResultFound:
+            raise ValueError
